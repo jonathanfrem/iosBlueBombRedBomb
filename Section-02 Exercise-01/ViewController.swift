ba293bce6864d0499ab8edcaced7ecbd3e2e2382
Jonathan Fremstad <jonathan@Jonathans-iMac.local>
2016-03-12 22:56:16 +0100

    added files

--- Section-02 Exercise-01/ViewController.swift
+++ Section-02 Exercise-01/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hideBlueBomb: UIButton!
+    @IBOutlet weak var hideRedBomb: UIButton!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var reset: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func hideBlueBomb_act(sender: AnyObject) {
+        blueBomb.hidden=true
+        redBomb.hidden=false
+        
+    }
+    
+    @IBAction func hideRedBomb_act(sender: AnyObject) {
+        redBomb.hidden=true
+        blueBomb.hidden=false
+    }
 
+    @IBAction func reset_act(sender: AnyObject) {
+        redBomb.hidden=false
+        blueBomb.hidden=false
+        
+    }
 }
 
