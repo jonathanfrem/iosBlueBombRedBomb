11627a7eea0df1ff443d5cd42baf685cc96a24e5
Jonathan Fremstad <jonathan@jonathans-imac.local>
2016-03-12 21:10:05 +0100

    Initial Commit

---
+++ Section-02 Exercise-01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Section-02 Exercise-01
+//
+//  Created by Jonathan Fremstad on 12/03/16.
+//  Copyright © 2016 Jonathan Fremstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
